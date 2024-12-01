8af786f6abc8a35e26b9d4a287e9b0b63986bc5f
Rajendra Pandey <rajendrapandey199971@gmail.com>
2024-12-01 17:27:37 +0545

    learned datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,38 @@
+"use strict"; // trest all JS code as new version of JS.(ES6)
+
+//alert( 3+3) // works on browser console.
+
+console.log(3 + 3);
+console.log("Raja");
+
+// Data Types in JavaScript:
+let name = "Raja"; // string
+let age = 25; // number(integer)
+let isLoggedIn = true; // boolean
+let email = null; // null
+let address; // undefined
+let data = {}; // object
+let numbers = [1, 2, 3, 4, 5]; // array
+
+// primitive data types:
+// number => 2 to the power of 53
+// BigInt => 2n to the power of 53
+// string => "Hello World"
+// boolean => true or false
+// null => null (standard value)
+// undefined => undefined
+// symbol => Symbol('description')
+
+// non-primitive data types:
+// object => {key: value}
+// array => [1, 2, 3, 4, 5]
+// function => function() {}
+
+// special data types:
+// NaN => Not a Number
+
+// To check data type of variable:
+console.log(typeof "Raja");
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
